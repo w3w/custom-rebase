42dee48c5b64732fb1fee27fc4dbd5a07b718dc5
Michal Vrchota <michal.vrchota@gmail.com>
2015-09-30 13:14:55 +0200

    Initial commit

--- touch-commit.php
+++ touch-commit.php
@@ -6,6 +6,12 @@ $commit = $argv[1];
 
 $commit = "e758f6395c16b558a0b2927d21095b6823b5e518";
 
+$commandMessage = sprintf('git log --format=%%B -n 1 %s', $commit);
+exec($commandMessage, $output);
+$commandMessage = $output[0];
+
+echo $commandMessage . PHP_EOL;
+
 $commandDiff = sprintf('git diff --name-only %s', $commit);
 
 $output = array();
@@ -20,4 +26,13 @@ foreach ($phpFiles as $phpFile) {
     convertFile($phpFile);
 }
 
-//$commandCherryPick = sprintf('git cherry-pick ')
+$commandCherryPick = sprintf('git cherry-pick %s', $commit);
+exec($commandCherryPick, $output);
+print_r($output);
+
+echo "Pausing command..." . PHP_EOL;
+fgetc(STDIN);
+
+foreach ($phpFiles as $phpFile) {
+    // exec $phpFile - revert short diffs
+}
